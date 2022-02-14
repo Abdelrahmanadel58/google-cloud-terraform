3d3941d3431648791621ab9366c71fcc7d49ea40
Abdelrahman Adel <84208921+Abdelrahmanadel58@users.noreply.github.com>
2022-02-14 14:26:49 +0200

    Terraform

--- gke.tf
+++ gke.tf
@@ -56,7 +56,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     machine_type = "e2-medium"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.gke.email
+    service_account = google_service_account.vm.email
     oauth_scopes    = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
